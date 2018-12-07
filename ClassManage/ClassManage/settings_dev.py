91a84c5c11571a87c7eb696b9cf466d85b7a7ee4
su <825800608@qq.com>
2018-12-07 12:43:54 +0800

    修改模型类

--- ClassManage/ClassManage/settings_dev.py
+++ ClassManage/ClassManage/settings_dev.py
@@ -25,7 +25,7 @@ SECRET_KEY = '(!j7z^i*ecv&8yn#6f)@mk#g_kh*wi_fye63u6=4z9!*z#96g6'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -103,9 +103,9 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/2.0/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'zh-Han'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'Asia/Shanghai'
 
 USE_I18N = True
 
--- ClassManage/main/models.py
+++ ClassManage/main/models.py
@@ -1,10 +1,15 @@
 import random
+import base64
 import hashlib
 
 from django.db import models
 
 
 def random_username():
+    """
+    获取随机用户名
+    :return: 随机用户名
+    """
     word = '1234567890qwertyuiopasdfghjklzxcvbnm'
     username = ''.join(random.sample(word, 8))
     return username
@@ -33,7 +38,7 @@ class TeacherInfo(models.Model):
     name = models.CharField(verbose_name='老师姓名', max_length=50)
     gender = models.BooleanField(verbose_name='性别', default=False)
     user = models.CharField(verbose_name='账号', max_length=50, default=random_username)
-    password = models.CharField(verbose_name='密码', max_length=40, default='1411678a0b9e25ee2f7c8b2f7ac92b6a74b3f9c5')
+    password = models.CharField(verbose_name='密码', max_length=40, default=encrypt_password('666666'))
 
     def __str__(self):
         return self.name
@@ -44,8 +49,16 @@ class TeacherInfo(models.Model):
         else:
             return '女'
 
-    def encrypt_passwd(self, passwd):
-        return hashlib.sha1(passwd.encode()).hexdigest()
+    @staticmethod
+    def encrypt_password(password):
+        """
+        加密密码
+        :param password: 明文密码
+        :return: 加密后的密码
+        """
+        m = hashlib.md5(password.encode()).hexdigest()
+        b = base64.b64encode(m.encode())
+        return hashlib.sha1(b).hexdigest()
 
     class Meta:
         verbose_name = '老师信息'
@@ -53,25 +66,29 @@ class TeacherInfo(models.Model):
 
 
 class SubjectInfo(models.Model):
+    """
+    课程种类，仅用于显示有多少种课程
+    """
     CLASS_TYPE = (
         (0, '晚辅'),
         (1, '一对一'),
         (2, '小班课'),
         (3, '兼职'),
     )
-    name = models.CharField(verbose_name='科目名称', max_length=50)
+    name = models.CharField(verbose_name='科目种类', max_length=50)
     class_type = models.IntegerField(verbose_name='科目类型', choices=CLASS_TYPE, default=CLASS_TYPE[1][0])
 
     def __str__(self):
         return self.name
 
     class Meta:
-        verbose_name = '科目信息'
+        verbose_name = '科目种类信息'
         verbose_name_plural = verbose_name
 
 
 class LessonInfo(models.Model):
     name = models.CharField(verbose_name='课程名称', max_length=50)
+    subject = models.ForeignKey(verbose_name='科目种类', to=SubjectInfo, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.name
@@ -81,24 +98,38 @@ class LessonInfo(models.Model):
         verbose_name_plural = verbose_name
 
 
-class StudentToLesson(models.Model):
-    lesson = models.ForeignKey(verbose_name='课程', to=LessonInfo, on_delete=models.CASCADE)
-    student = models.ForeignKey(verbose_name='学生', to=StudentInfo, on_delete=models.CASCADE)
+class StudentOfLesson(models.Model):
+    lesson = models.ForeignKey(verbose_name='课程信息', to=LessonInfo, on_delete=models.CASCADE)
+    student = models.ForeignKey(verbose_name='学生信息', to=StudentInfo, on_delete=models.CASCADE)
+    price = models.DecimalField(verbose_name='学生课时费', max_digits=18, decimal_places=2)
 
     def __str__(self):
-        return '%s的%s' % (self.student.name, self.lesson.name)
+        return self.lesson.name
 
     class Meta:
         verbose_name = '学生课程关系表'
         verbose_name_plural = verbose_name
 
 
-class ClassPrice(models.Model):
-    lesson = models.ForeignKey(verbose_name='课程', to=LessonInfo, on_delete=models.CASCADE)
-    student = models.ForeignKey(verbose_name='学生', to=StudentInfo, on_delete=models.CASCADE)
-    teacher = models.ForeignKey(verbose_name='老师', to=TeacherInfo, on_delete=models.CASCADE)
-    price = models.DecimalField(verbose_name='价格', max_digits=18, decimal_places=2)
+class TeacherOfLesson(models.Model):
+    # lesson = models.ForeignKey(verbose_name='学生课程信息', to=StudentOfLesson, on_delete=models.CASCADE)
+    lesson = models.ForeignKey(verbose_name='课程信息', to=LessonInfo, on_delete=models.CASCADE)
+    teacher = models.ForeignKey(verbose_name='老师信息', to=TeacherInfo, on_delete=models.CASCADE)
+    price = models.DecimalField(verbose_name='老师课时费', max_digits=18, decimal_places=2)
+
+    def __str__(self):
+        return '%s[%s]' % (self.teacher, self.lesson)
 
     class Meta:
-        verbose_name = '课程价格'
+        verbose_name = '老师课程关系表'
+        verbose_name_plural = verbose_name
+
+
+class ScheduleLessonInfo(models.Model):
+    lesson = models.ForeignKey(verbose_name='课程', to=TeacherOfLesson, on_delete=models.CASCADE)
+    schedule = models.DateTimeField(verbose_name='上课时间')
+    hour = models.DecimalField(verbose_name='课时', default=2)
+
+    class Meta:
+        verbose_name = '排课表'
         verbose_name_plural = verbose_name
