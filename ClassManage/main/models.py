8e93e812891a089a6f5b9d3131246a1b6c23845c
su <825800608@qq.com>
2018-12-09 15:59:39 +0800

    添加h5页面

--- ClassManage/main/models.py
+++ ClassManage/main/models.py
@@ -16,9 +16,25 @@ def random_username():
 
 
 class StudentInfo(models.Model):
+    GRADE = (
+        (0, '其他'),
+        (1, '一年级'),
+        (2, '二年级'),
+        (3, '三年级'),
+        (4, '四年级'),
+        (5, '五年级'),
+        (6, '六年级'),
+        (7, '初一'),
+        (8, '初二'),
+        (9, '初三'),
+        (10, '高一'),
+        (11, '高二'),
+        (12, '高三'),
+    )
     name = models.CharField(verbose_name='学生姓名', max_length=20)
     gender = models.BooleanField(verbose_name='性别', default=False)
     age = models.IntegerField(verbose_name='年龄', null=True, blank=True)
+    grade = models.IntegerField(verbose_name='年级', choices=GRADE, default=GRADE[4][0], null=True, blank=True)
 
     def __str__(self):
         return self.name
