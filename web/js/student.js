a9a95ad0cb0f4cceb063d8fb2d499a7b3f72b859
su <825800608@qq.com>
2018-12-10 23:28:19 +0800

    修改添加学生的js

--- web/js/student.js
+++ web/js/student.js
@@ -2,8 +2,6 @@ $(function () {
 	var error_name = false;
 	var error_age = false;
 
-	var age = $('#age')
-
 	$('#name').blur(function () {
 		check_name();
 	})
@@ -12,14 +10,46 @@ $(function () {
 		check_age();
 	})
 
-	function check_age(){
-		if(age=='') {
-			return
+	function check_name(){
+		if($('#name').val()==''){
+			$('#name').parent().parent().next().html('请输入学生名').show()
+			error_name = true;
 		}
 		else{
-			if (!(/(^[1-9]\d*$)/.test(age))) {
+			$('#name').parent().parent().next().hide()
+			error_name = false;
+		}
+	}
+
+	function check_age(){
+		if (!(/^\d*$/).test($('#age').val())) {
+			$('#age').parent().parent().next().html('请输入整数').show()
+			error_age = true
+		}
+		else{
+			var age = Number($('#age').val())
+			if (age<0||age>800) {
+				$('#age').parent().parent().next().html('请输入正确的年龄').show()
 				error_age = true
+			}
+			else{
+				$('#age').parent().parent().next().hide()
+				error_age = false
 			}
 		}
 	};
+
+	$('#refer_to').click(function() {
+		check_name();
+		check_age();
+
+		var name = $('#name').val();
+		var gender = $('#gender').val();
+		var grade = $('#grade').val();
+		var age = $('#age').val();
+
+		$.post('url', {'name': name, 'gender': gender, 'grade': grade, 'age': age}, function(result) {
+			alert(result);
+		})
+	})
 })
